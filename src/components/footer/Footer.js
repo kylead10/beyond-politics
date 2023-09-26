0db04710d5011f3c8f01833f45fb95b0a3fbb92e
kylead10 <adiokayode111@gmail.com>
2023-09-26 22:25:38 +0100

    footer design

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  margin: 0;
+  padding: 0;
+  font-size: 50px;
+  text-align: center;
+  background-color: grey;
+`;
+
+const StyledH1 = styled.h1`
+  margin: 0;
+  padding: 0;
+  font-size: 50px;
+  text-align: center;
+  background-color: grey;
+`;
 
 const Footer = () => {
   return (
     <>
       <section>
-        <h1>I am Footer</h1>
+        <FooterContainer></FooterContainer>
+        <StyledH1>footer</StyledH1>
       </section>
     </>
   );
