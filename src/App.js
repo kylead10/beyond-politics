57640b8d67f48bc3bee55ffa8a11825399c2f556
kylead10 <adiokayode111@gmail.com>
2023-10-01 22:30:35 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/header/Header';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
 import Pop from './components/registerModal/Pop';
-import RegistrationForm from './components/registerModal/Register';
+// import RegistrationForm from './components/RegistrationForm';
 
 const App = () => {
   return (
