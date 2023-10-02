b6f6d93438d2c56332d962e7faad5568b91edf7b
kylead10 <adiokayode111@gmail.com>
2023-10-02 22:02:45 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/header/Header';
 import About from './components/about/About';
 import Footer from './components/footer/Footer';
 import Pop from './components/registerModal/Pop';
-// import RegistrationForm from './components/RegistrationForm';
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
       <About />
       <Pop />
       <Footer />
-      {/* <RegistrationForm /> */}
     </>
   );
 };
